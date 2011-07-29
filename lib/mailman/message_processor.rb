42e4171d86df81b5b5806e717b8549602f38f476
Cyril Mougel <cyril.mougel@gmail.com>
2011-07-29 11:52:42 +0200

    process the message before move to seen in Maildir to avoid not process if failure

--- lib/mailman/message_processor.rb
+++ lib/mailman/message_processor.rb
@@ -18,11 +18,18 @@ module Mailman
       @router.route(message)
     end
 
+    ##
     # Processes a +Maildir::Message+ instance.
+    # Move mail from new to cur directory of Maildir
+    #
+    # If process failed the Mail is not move and mark like see
+    #
+    # @params[MailDir::Message] message the mail get from Maildir
+    #
     def process_maildir_message(message)
+      process(message.data)
       message.process # move message to cur
       message.seen!
-      process(message.data)
     end
 
   end
